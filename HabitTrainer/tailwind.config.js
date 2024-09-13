6ff606f1cb6318fce7d670218eef2eb87ffa8a9d
Dietmar Braun <dbraun1985@gmail.com>
2024-09-13 17:31:40 +0200

    (feat: tailwindCSS) Integrate TailwindCSS for razor files

---
+++ HabitTrainer/tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ['./**/*.{razor,html}'],
+    darkMode: 'class',
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+}

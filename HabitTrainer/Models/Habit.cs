7cce3179ff689ca8f397052ec8cb8759c6a9a1a0
Dietmar Braun <dbraun1985@gmail.com>
2024-09-17 22:48:21 +0200

    (feat: Habit Model) Extend model with new properties & define JSON property names for object items

--- HabitTrainer/Models/Habit.cs
+++ HabitTrainer/Models/Habit.cs
@@ -4,10 +4,41 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using Newtonsoft;
+using Newtonsoft.Json;
+
 namespace HabitTrainer.Models
 {
+    public enum RepetitionInterval {
+        Daily,
+        Weekly,
+        Monthly
+    };
+
+    public class Repetition
+    {
+        [JsonProperty("interval")]
+        public RepetitionInterval? Interval { get; set; }
+
+        [JsonProperty("repetitions")]
+        public int Repetitions { get; set; } = 0;
+    } 
+
+    public class Reminder
+    {
+        [JsonProperty("time")]
+        public TimeOnly Time { get; set; }
+
+    }
+
     public class Habit
     {
-        public string Name { get; set; }
+        [JsonProperty("name")]
+        public string? Name { get; set; }
+
+        [JsonProperty("repetition")]
+        public Repetition? Repetition { get; set; }
+
+        public IEnumerable<Reminder>? Reminders { get; set; }
     }
 }
